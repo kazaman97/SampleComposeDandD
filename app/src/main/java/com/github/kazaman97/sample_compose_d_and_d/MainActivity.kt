bb9992e7cf1c8cf6fb7ab8552e1367c99f7fa546
kazaman97 <narusai97@gmail.com>
2023-06-10 23:04:56 +0900

    add swipetodismiss to sample item

--- app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
+++ app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,12 +16,28 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
 import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.kazaman97.sample_compose_d_and_d.ui.theme.SampleComposeDandDTheme
@@ -32,12 +52,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    var samples by remember {
+                        mutableStateOf(
+                            (1..100).map {
+                                Sample(
+                                    id = it.toLong(),
+                                    text = "id: $it"
+                                )
+                            }
+                        )
+                    }
+
                     SampleList(
-                        samples = (1..10).map {
-                            Sample(
-                                id = it.toLong(),
-                                text = "id: $it"
-                            )
+                        samples = samples,
+                        onDelete = { id ->
+                            samples = samples.toMutableList().apply {
+                                removeIf { it.id == id }
+                            }
                         }
                     )
                 }
@@ -51,8 +82,12 @@ private data class Sample(
     val text: String
 )
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-private fun SampleList(samples: List<Sample>) {
+private fun SampleList(
+    samples: List<Sample>,
+    onDelete: (id: Long) -> Unit
+) {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -60,7 +95,59 @@ private fun SampleList(samples: List<Sample>) {
             items = samples,
             key = { it.id }
         ) { sample ->
-            SampleItem(text = sample.text)
+            val dismissState = rememberDismissState(
+                confirmStateChange = {
+                    if (it == DismissValue.DismissedToStart) onDelete(sample.id)
+                    true
+                }
+            )
+
+            SwipeToDismiss(
+                state = dismissState,
+                directions = setOf(DismissDirection.EndToStart),
+                background = {
+                    val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+                    val color by animateColorAsState(
+                        targetValue = when (dismissState.targetValue) {
+                            DismissValue.Default -> Color.LightGray
+                            DismissValue.DismissedToEnd -> Color.Green
+                            DismissValue.DismissedToStart -> Color.Red
+                        }
+                    )
+                    val alignment = when (direction) {
+                        DismissDirection.StartToEnd -> Alignment.CenterStart
+                        DismissDirection.EndToStart -> Alignment.CenterEnd
+                    }
+                    val icon = when (direction) {
+                        DismissDirection.StartToEnd -> Icons.Default.Done
+                        DismissDirection.EndToStart -> Icons.Default.Delete
+                    }
+                    val scale by animateFloatAsState(
+                        targetValue = if (dismissState.targetValue == DismissValue.Default) {
+                            0.75f
+                        } else {
+                            1f
+                        }
+                    )
+
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .background(color)
+                            .padding(horizontal = 20.dp),
+                        contentAlignment = alignment
+                    ) {
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = "Localized description",
+                            modifier = Modifier.scale(scale)
+                        )
+                    }
+                },
+                dismissContent = {
+                    SampleItem(text = sample.text)
+                }
+            )
             Divider()
         }
     }
@@ -71,6 +158,7 @@ private fun SampleItem(text: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .background(Color.White)
             .padding(horizontal = 4.dp)
             .heightIn(min = 48.dp),
         verticalArrangement = Arrangement.Center
