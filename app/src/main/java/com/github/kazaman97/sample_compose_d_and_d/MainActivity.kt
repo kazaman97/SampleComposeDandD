e2cd3c1fa24351784e96703a25f5d65f748ce753
kazaman97 <narusai97@gmail.com>
2023-06-10 22:39:40 +0900

    downgrade to material from material3

--- app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
+++ app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
@@ -12,10 +12,10 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
                     SampleList(
                         samples = (1..10).map {
