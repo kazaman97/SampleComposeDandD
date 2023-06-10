194fbd4f2fbc9292a72e92bb63a1edffdc65ed1b
kazaman97 <narusai97@gmail.com>
2023-06-10 22:17:33 +0900

    add sample item component

--- app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
+++ app/src/main/java/com/github/kazaman97/sample_compose_d_and_d/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.github.kazaman97.sample_compose_d_and_d
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.kazaman97.sample_compose_d_and_d.ui.theme.SampleComposeDandDTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +32,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SampleList(
+                        samples = (1..10).map {
+                            Sample(
+                                id = it.toLong(),
+                                text = "id: $it"
+                            )
+                        }
+                    )
                 }
             }
         }
     }
 }
 
+private data class Sample(
+    val id: Long,
+    val text: String
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun SampleList(samples: List<Sample>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(
+            items = samples,
+            key = { it.id }
+        ) { sample ->
+            SampleItem(text = sample.text)
+            Divider()
+        }
+    }
+}
+
+@Composable
+private fun SampleItem(text: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 4.dp)
+            .heightIn(min = 48.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = text)
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun GreetingPreview() {
+private fun SampleItemPreview() {
     SampleComposeDandDTheme {
-        Greeting("Android")
+        SampleItem("id: 1")
     }
 }
